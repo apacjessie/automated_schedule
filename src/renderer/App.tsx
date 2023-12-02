6e8252e21e4e88254ed552258bddc8f9aee86cfa
apacjessie <apacjessie@gmail.com>
2023-12-02 22:01:39 +0800

    Rename AppProvider to AppProviders

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,12 +1,11 @@
 import './App.css';
-import PageRoute from './pages/PageRoute';
-import AppProvider from './providers/app';
+import AppProviders from './providers/app';
 import AppRoutes from './routes';
 
 export default function App() {
   return (
-    <AppProvider>
+    <AppProviders>
       <AppRoutes />
-    </AppProvider>
+    </AppProviders>
   );
 }
--- src/renderer/providers/app.tsx
+++ src/renderer/providers/app.tsx
@@ -1,8 +1,8 @@
 import { ReactNode } from 'react';
 import { MemoryRouter as Router } from 'react-router-dom';
 
-const AppProvider = ({ children }: { children: ReactNode }) => {
+const AppProviders = ({ children }: { children: ReactNode }) => {
   return <Router>{children}</Router>;
 };
 
-export default AppProvider;
+export default AppProviders;
