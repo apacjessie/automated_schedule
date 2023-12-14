44784ef737d5a34e03bf979ffbf2fa3b4d08a571
apacjessie <apacjessie@gmail.com>
2023-12-14 17:48:28 +0800

    Refactor

--- src/renderer/routes/index.tsx
+++ src/renderer/routes/index.tsx
@@ -1,18 +1,17 @@
 import { useNavigate, useRoutes } from 'react-router-dom';
 import { useEffect } from 'react';
+import auth from '@/utils/auth';
 import publicRoutes from './public';
 import privateRoutes from './private';
-
-const auth = true;
 
 const AppRoutes = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (auth) navigate('/dashboard');
+    if (auth.authenticated) navigate('/dashboard');
   }, [navigate]);
 
-  const route = auth ? privateRoutes : publicRoutes;
+  const route = auth.authenticated ? privateRoutes : publicRoutes;
 
   return useRoutes(route);
 };
