4e64123ec9e90bed4eef36681d33c2640692269b
apacjessie <apacjessie@gmail.com>
2023-12-21 19:03:24 +0800

    extend ts color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,15 @@ module.exports = {
     fontFamily: {
       poppins: ['Poppins', 'sans-serif', 'system-ui'],
     },
-    colors: {
-      base: '#000A2F',
-      white: '#fff',
-      black: '#000',
-      slate: '#cbd5e1',
-      accent: '#2563eb',
-      hovers: '#f1f5f9',
+    extend: {
+      colors: {
+        base: '#000A2F',
+        white: '#fff',
+        black: '#000',
+        slate: '#cbd5e1',
+        accent: '#2563eb',
+        hovers: '#f1f5f9',
+      },
     },
   },
   variants: {},
